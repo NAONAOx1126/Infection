e18ce43beb9355b4525a532d6dc03ab0d569c595
Naohisa Minagawa <info@clay-system.jp>
2014-08-11 17:55:10 +0900

    ドメイン毎の設定ファイルを追加

--- _configure/configure_twt-system.com.php
+++ _configure/configure_twt-system.com.php
@@ -44,14 +44,4 @@ Vizualizer_Configure::set("refollow_enabled", false);
 
 // デフォルト設定を利用するカラム
 Vizualizer_Configure::set("twitter_default_setting_keys", array(
-    "japanese_flg", "unlock_user_flg", "unique_icon_flg", "non_bot_flg",
-    "follow_ratio_1", "daily_follows_1", "daily_unfollows_1",
-    "follower_limit_2", "follow_ratio_2", "daily_follows_2", "daily_unfollows_2",
-    "follower_limit_3", "follow_ratio_3", "daily_follows_3", "daily_unfollows_3",
-    "follower_limit_4", "follow_ratio_4", "daily_follows_4", "daily_unfollows_4",
-    "follower_limit_5", "follow_ratio_5", "daily_follows_5", "daily_unfollows_5",
-    "follower_limit_6", "follow_ratio_6", "daily_follows_6", "daily_unfollows_6",
-    "follower_limit_7", "follow_ratio_7", "daily_follows_7", "daily_unfollows_7",
-    "follower_limit_8", "follow_ratio_8", "daily_follows_8", "daily_unfollows_8",
-    "follower_limit_9", "follow_ratio_9", "daily_follows_9", "daily_unfollows_9"
 ));
