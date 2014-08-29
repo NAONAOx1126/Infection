bb8969d590a7effebb474b715a8c1f0b7853d708
Naohisa Minagawa <info@clay-system.jp>
2014-08-30 00:47:48 +0900

    設定ファイルを変更

--- _configure/configure_twitter3.sweetberry.jp.php
+++ _configure/configure_twitter3.sweetberry.jp.php
@@ -6,7 +6,7 @@ Vizualizer_Configure::set("timezone", "Asia/Tokyo");
 Vizualizer_Configure::set("site_code", "twitter");
 
 // サイト名
-Vizualizer_Configure::set("site_name", "NEO CHERRY");
+Vizualizer_Configure::set("site_name", "ツイッターツール");
 
 // サイトメールアドレス
 Vizualizer_Configure::set("site_email", "info@sweetberry.jp");
@@ -15,7 +15,7 @@ Vizualizer_Configure::set("site_email", "info@sweetberry.jp");
 Vizualizer_Configure::set("site_domain", $_SERVER["SERVER_NAME"]);
 
 // サイトホームディレクトリ
-Vizualizer_Configure::set("site_home", realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . "..") . DIRECTORY_SEPARATOR . "templates");
+Vizualizer_Configure::set("site_home", realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . "..") . DIRECTORY_SEPARATOR . "templates2");
 
 // データベースの接続設定
 Vizualizer_Configure::set("database", array(
